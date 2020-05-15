4be657349f3768d429a57f6076eaa41a40b7b3e8
agilebala <64301804+agilebala@users.noreply.github.com>
2020-05-14 23:14:38 -0700

    data set modified.

--- ca/demo/salestool/SalesData.java
+++ ca/demo/salestool/SalesData.java
@@ -1,7 +1,7 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	private int data[]= {0,4,42};
+	private int data[]= {0,4,42,22};
 	public void display() {
 		System.out.println("TODO: Implement Data display");
 	}
